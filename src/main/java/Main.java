59d4982e68a9984bdada9bc25661a2e7e9e8b12e
David Ausweger <david.ausweger@porscheinformatik.at>
2018-11-23 13:08:07 +0100

    delete IJ stuff

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,9 @@
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
-
-import com.sun.org.apache.bcel.internal.util.ClassPath;
 
 public class Main
 {
@@ -15,11 +11,12 @@ public class Main
     public static void main(String args[]) throws IOException
     {
 
-//        InputStream is= ClassLoader.getSystemResourceAsStream("morse.txt");
-//
-//        BufferedInputStream stream = new BufferedInputStream(is);
+        InputStream is= ClassLoader.getSystemResourceAsStream("morse.txt");
 
-        Reader morseReader = new MorseReader(new BufferedReader(new FileReader("c:\\temp\\morse.txt")));
+        BufferedInputStream stream = new BufferedInputStream(is);
+
+//        Reader morseReader = new MorseReader(new BufferedReader(new FileReader("c:\\temp\\morse.txt")));
+        Reader morseReader = new MorseReader(new BufferedReader(new InputStreamReader(is)));
 
         System.out.println(morseReader.read());
 
